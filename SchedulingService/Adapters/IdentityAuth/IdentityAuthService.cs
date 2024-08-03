8c552fb4f24b4c2316cd230b51d893c4497878d9
Paulo Silva <pauloecomercial367@gmail.com>
2024-08-03 10:31:21 -0300

    :sparkles: Identity auth service implementation

--- SchedulingService/Adapters/IdentityAuth/IdentityAuthService.cs
+++ SchedulingService/Adapters/IdentityAuth/IdentityAuthService.cs
@@ -2,6 +2,7 @@ using Application.Auth.Ports;
 using Application.Auth.Request;
 using Application.Auth.Response;
 using Application.User.Response;
+using IdentityAuth.Exceptions;
 using IdentityAuth.Jwt;
 using Microsoft.AspNetCore.Identity;
 
@@ -12,28 +13,92 @@ public class IdentityAuthService : IAuthUserService
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
     private readonly JwtGenerator _jwtGenerator;
-    public Task<LoggedUserResponse> AuthenticateAsync(LoginUserRequest request)
+    public async Task<LoggedUserResponse> AuthenticateAsync(LoginUserRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, false, true);
+        if (result.Succeeded) return await _jwtGenerator.GerarToken(request.Email);
+
+        var userAuth = new LoggedUserResponse();
+        if (!result.Succeeded)
+        {
+            if (result.IsLockedOut)
+                userAuth.AddError("Your account is blocked");
+            else if (result.IsNotAllowed)
+                userAuth.AddError("You don't have permission to do this actions");
+            else if (result.RequiresTwoFactor)
+                userAuth.AddError("You need to confirm 2 factor code");
+            else
+                userAuth.AddError("User or password is invalid");
+        }
+        return userAuth;
     }
 
-    public Task<RegisteredUserResponse> RegisterAsync(RegisterUserRequest register)
+    public async Task<RegisteredUserResponse> RegisterAsync(RegisterUserRequest register)
     {
-        throw new NotImplementedException();
+        var identityUser = new IdentityUser
+        {
+            UserName = register.Email,
+            Email = register.Email,
+            EmailConfirmed = true,
+        };
+        var result = await _userManager.CreateAsync(identityUser, register.Password);
+        var response = new RegisteredUserResponse();
+        if (result.Succeeded)
+        {
+            await _userManager.SetLockoutEnabledAsync(identityUser, false);
+            response.UserEmail = register.Email;
+
+            var roleResult = await _userManager.AddToRoleAsync(identityUser, register.Role.ToString());
+
+        }
+        else
+            response.SetError(result.Errors.ToList().Select(r => r.Description).ToList());
+
+        return response;
     }
 
-    public Task UpdateAuthUserAsync(Domain.Entities.User user, UpdateUserRequest request)
+    public async Task UpdateAuthUserAsync(Domain.Entities.User user, UpdateUserRequest request)
     {
-        throw new NotImplementedException();
+        var identityUser = await _userManager.FindByEmailAsync(user.Email);
+
+        if (identityUser == null)
+            throw new AuthUserNotFoundException("Auth user was not founded");
+
+        if (!string.IsNullOrEmpty(request.Email))
+        {
+            identityUser.Email = request.Email;
+            identityUser.UserName = request.Email;
+            user.Email = request.Email;
+
+            var emailUpdateResult = await _userManager.UpdateAsync(identityUser);
+            if (!emailUpdateResult.Succeeded)
+                throw new Exception("Error when try to update user");
+        }
+
+        if (!string.IsNullOrEmpty(request.Password))
+        {
+            var token = await _userManager.GeneratePasswordResetTokenAsync(identityUser);
+            var passwordUpdateResult = await _userManager.ResetPasswordAsync(identityUser, token, request.Password);
+            if (!passwordUpdateResult.Succeeded)
+                throw new Exception("Error when try to update auth user password");
+        }
+
     }
 
-    public Task DeleteAsync(Domain.Entities.User user)
+    public async Task DeleteAsync(Domain.Entities.User user)
     {
-        throw new NotImplementedException();
+        var identityUser = await _userManager.FindByEmailAsync(user.Email);
+
+        if (identityUser == null)
+            throw new AuthUserNotFoundException("Auth user was not founded");
+
+        await _userManager.DeleteAsync(identityUser);
     }
 
-    public Task<UserDto?> GetOneByIdAsync(Guid id)
+    public async Task<UserDto?> GetOneByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var user = await _userManager.FindByIdAsync(id.ToString());
+        if (user == null) return null;
+        return new UserDto{Email = user.Email, Name = user.UserName, Id = id};
     }
 }
