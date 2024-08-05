241052bed99f94e233550eb25090a0c86d52a9b5
Paulo Silva <pauloecomercial367@gmail.com>
2024-08-05 08:50:42 -0300

    :rotating_light: Should not create new professional user if auth service return errors

--- SchedulingService/Tests/ApplicationTests/UserServiceTests.cs
+++ SchedulingService/Tests/ApplicationTests/UserServiceTests.cs
@@ -67,7 +67,32 @@ public class UserServiceTests
         var created = await userService.CreateUserAsync(request);
         Assert.AreNotEqual(0, created.Errors.Count);
     }
-
+    [Test]
+    public async Task ShouldNotCreateNewProfessionalUserIfAuthServiceReturnErrors()
+    {
+        var createdUserId = Guid.NewGuid();
+        var request = new CreateUserRequest
+        {
+            Email = "email@email.com",
+            Name = "Nome",
+            Password = "Senha@123123",
+            ConfirmPassword = "Senha@123123"
+        };
+        request.SetRole(Roles.Professional);
+        var authResponse = new RegisteredUserResponse();
+        authResponse.AddErrors("Error 1");
+        
+        _authUserService.Setup(au => au.RegisterAsync(It.IsAny<RegisterUserRequest>()))
+            .ReturnsAsync(authResponse);
+        _userRepository.Setup(us => us.CreateAsync(It.IsAny<User>()))
+            .ReturnsAsync(new Professional { Id = createdUserId, Name = "Nome", Email = "email@email.com" });
+        var userService = new UserService(_userRepository.Object, _authUserService.Object);
+        var created = await userService.CreateUserAsync(request);
+        var errorsQuantityExpected = 1;
+        
+        Assert.AreEqual(created.User, null);
+        Assert.AreEqual(created.Errors.Count, errorsQuantityExpected);
+    }
 
     [Test]
     public async Task ShouldCreateNewAdminUser()
